a19534966dd35c1a94a462f52a933dfaa5691ff9
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2021-01-15 15:43:14 +0100

    Initial commit of SpringBoot Initializr project

---
+++ src/main/java/nl/maastrichtuniversity/cds/modelcommissioningstation/ModelCommissioningStationApplication.java
@@ -0,0 +1,13 @@
+package nl.maastrichtuniversity.cds.modelcommissioningstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelCommissioningStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelCommissioningStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/maastrichtuniversity/cds/modelcommissioningstation/ModelCommissioningStationApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.maastrichtuniversity.cds.modelcommissioningstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelCommissioningStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
