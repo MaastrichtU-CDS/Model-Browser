86c9e866a179ebee5a00c066e9dd756a95fa2123
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2021-01-15 16:06:31 +0100

    Added basic controller and webpage (bootstrap)

---
+++ src/main/java/nl/maastrichtuniversity/cds/modelcommissioningstation/controller/MainController.java
@@ -0,0 +1,14 @@
+package nl.maastrichtuniversity.cds.modelcommissioningstation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
