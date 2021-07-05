a0619dc897617f4f343913523e3465702fea7a47
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2021-07-05 12:19:24 +0200

    Disabled override to check classpath only

--- src/main/java/nl/maastrichtuniversity/cds/modelcommissioningstation/helperObjects/AppProperties.java
+++ src/main/java/nl/maastrichtuniversity/cds/modelcommissioningstation/helperObjects/AppProperties.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+//@PropertySource("classpath:application.properties")
 public class AppProperties {
     /////////////////////////Validation repository properties/////////////////////////
     //////////////////////////////////////////////////////////////////////////////////
